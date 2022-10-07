618365f4ece73ab38c2fabd3670094716e38835d
DevIncomin <56730075+Developer-Incoming@users.noreply.github.com>
2022-10-07 15:23:32 +0300

    Update main.py
    Tried to do something, but then I just gave up, because I had stuff to do more important than this 💀

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
+from curses import KEY_SPREVIOUS
 import keyboard
 
+# Key events stored here
 event_counter = []
 
+# For counting how many keys pressed before writing it into the txt document
+keysPressed = 0
 
+# After {writeEventsAfter} keys pressed, it'll write everything into a file
+writeEventsAfter = 100
+
+# Saves all events into "input_counter.txt"
+def saveKeysPressed():
+    print("dump")
+
+    dataFile = open("input_counter.txt", "w")
+    dataFile.write("\n" + str(keysPressed / 2))
+    dataFile.close()
+
+    event_counter.clear()
+
+# Gets called whenever the user clicks any key."
 def on_press(event):
-	event_counter.append(event)
-	with open('input_counter.txt', 'w') as file:
-		file.write('{} \n'.format(int(len(event_counter)/2)))
+    global keysPressed
+    keysPressed += 1
+
+    if keysPressed >= writeEventsAfter:
+        saveKeysPressed()
+        keysPressed = 0
 
 
 keyboard.hook(on_press)
 keyboard.wait()
-
-
